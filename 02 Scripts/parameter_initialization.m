1ab4a00a034c49746d3612624da7f545ba4cea86
Vasudev Purohit <vasudev.purohit@gmail.com>
2023-11-13 15:11:58 -0500

    Updated the Simulink model and the safety constraints function
    --Added the correct translations to track the vehicle footprint along the vehicle trajectory
    --added the same in the safety constraints functions to calculate the vehicle footprint over the horizon

--- 02 Scripts/parameter_initialization.m
+++ 02 Scripts/parameter_initialization.m
@@ -1,3 +1,5 @@
+clear all
+clc
 %main script that populates all the parameters neccessary to run the model
 %vehicle properties
 vehProps.m=1550;
@@ -8,19 +10,19 @@ vehProps.CMz1=2*0.13*1e3*180/pi;    % Nm/rad, per axle
 vehProps.CMz2=2*0.06*1e3*180/pi;    % Nm/rad, per axle
 vehProps.a=1.064;                   % m,front axle distance
 vehProps.b=1.596;                   %m, rear axle distance
-vehProps.V=70/3.6;                  %m/s, vehicle velocity
+vehProps.V=75/3.6;                  %m/s, vehicle velocity
 vehProps.footprint.x_fr=vehProps.a; %m, front right corner X
 vehProps.footprint.x_fl=vehProps.a; %m, front left corner X
 vehProps.footprint.x_rr=-vehProps.b;%m, rear right corner X
 vehProps.footprint.x_rl=-vehProps.b;%m, rear left corner X
-vehProps.footprint.y_fr=1.52/2;     %m, front right corner Y
-vehProps.footprint.y_fl=1.52/2;     %m, front left corner Y
-vehProps.footprint.y_rr=1.52/2;     %m, rear right corner Y
-vehProps.footprint.y_rl=1.52/2;     %m, rear left corner Y
+vehProps.footprint.y_fr=(-1.52/2);    %m, front right corner Y
+vehProps.footprint.y_fl=(1.52/2);     %m, front left corner Y
+vehProps.footprint.y_rr=(-1.52/2);    %m, rear right corner Y
+vehProps.footprint.y_rl=(1.52/2);     %m, rear left corner Y
 %driver properties
 driverProps.tau=0.15;               %driver lag time constant
 driverProps.Kd=0.01;                %Nm/rad - driver gain
-driverProps.La=22;                  %m, look-ahead distance
+driverProps.La=21;                  %m, look-ahead distance
 %rmpc properties
 rmpcProps.Aieq=1;                  %inequality constraint A matrix
 rmpcProps.Bieq=1;                  %inequality constraint B matrix
@@ -29,12 +31,18 @@ rmpcProps.Beq=1;                   %equality constraint B matrix
 rmpcProps.lb=1;                    %decision variable lower bound
 rmpcProps.ub=1;                    %decision variable upper bound
 %reference trajectory properties
-% load('refTraj.mat');
+load('refTraj.mat');
 %% checking results
 figure(1)
+cla reset
+axis equal
 plot(refTraj.X,refTraj.Y,'r--');
 hold on
 plot(out.X,out.Y,'k-');
+for i = (1:5:length(out.footprint))
+    patch([out.footprint(i,1) out.footprint(i,2) out.footprint(i,4) out.footprint(i,3)],[out.footprint(i,5) out.footprint(i,6) out.footprint(i,8) out.footprint(i,7)],'black');
+end
+xlim([20 65]);
 xlabel('X');
 ylabel('Y');
-legend("Reference Trajectory","Vehicle Trajectory",'Location','southoutside');
+% legend("Reference Trajectory","Vehicle Trajectory",'Location','southoutside');
